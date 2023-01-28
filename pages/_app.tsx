50d6b33138a07e8c0943b31b375ac4cbad9b1fc5
Luis Cruz <luiscruzdeveloper23@gmail.com>
2023-01-27 22:14:26 -0600

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import '@/styles/globals.css'
+import '@tremor/react/dist/esm/tremor.css'
+
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
---
+++ types/index.d.ts
@@ -0,0 +1,12 @@
+type Kpi = {
+    title: string,
+    quantity: number,
+}
+
+type User = {
+    name: string,
+    date: string,
+    rama: string,
+    distance: string,
+
+}
